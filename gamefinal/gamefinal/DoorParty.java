28c595ceaa2cbca18dbfee4fc55075d20805a82d
BubberDunk <BubberDunk@users.noreply.github.com>
2020-12-07 22:28:13 -0800

    Edits made by Sam

--- gamefinal/gamefinal/DoorParty.java
+++ gamefinal/gamefinal/DoorParty.java
@@ -10,18 +10,18 @@ public class DoorParty {
 	final static String DOOR1ITEMS = "";
 	final static String DOOR2PARTY = ""; //fill these in in the template of door3
 	final static String DOOR2ITEMS = "";
-	final static String DOOR3PARTY = "You press down on the absurd door handle and enter the room. The interior is... well, I don't know what you expected.\n"
+	final static String DOOR3PARTY = "\nYou press down on the absurd door handle and enter the room. The interior is... well, I don't know what you expected.\n"
 			+ "You're the one who picked the Pop-tart room. The walls are a diabetic shade of pink; the floor is bubblegum shag carpet.\n"
 			+ "Every square inch of the space is covered in cat paraphernalia. Cat posters on the walls, cat plushies on the floor, etc.\n"
 			+ "You look around, but are relieved to see no actual cats. Those would make a mess.\n";
-	final static String DOOR3ITEMS = "On the far wall, the tail of a cat clock swings back and forth, its eyes flicking with the trajectory.\n"
-			+ "In the center of the room, there's a round, blue kiddy pool full of cat-shaped beanbags.\n"
-			+ "To your right, there's a hole disappearing into the shag carpet. The interior of the hole also appears to be carpeted.\n"
-			+ "And, of course, there's the trivia machine.\n";
+	final static String DOOR3ITEMS = "On the far wall, the tail of a cat CLOCK swings back and forth, its eyes flicking with the trajectory.\n"
+			+ "In the center of the room, there's a round, blue kiddy POOL full of cat-shaped beanbags.\n"
+			+ "To your right, there's a HOLE disappearing into the shag carpet. The interior of the hole also appears to be carpeted.\n"
+			+ "And, of course, there's the TRIVIA machine.\n"; //maybe emphasize things you can examine with caps?
 	final static String DOOR4PARTY = ""; //fill these in in the template of door3
 	final static String DOOR4ITEMS = "";
-	final static String ITEMPROMPT = "Several items catch your attention:\n";
-	final static String ITEMINTERACT = "Do you want to look at any of them? Type 'exit' to exit.\n";
+	final static String ITEMPROMPT = "\nSeveral items catch your attention:\n";
+	final static String ITEMINTERACT = "\nDo you want to look at anything? Type help to pull up a list of things you can do.\n";
 	
 	
 	public static void door3Intro() throws FileNotFoundException {
@@ -32,25 +32,28 @@ public class DoorParty {
 	
 	public static void door3Interact(String investigate) throws FileNotFoundException {
 		String userInput;
-		
+
 		if (investigate.contains("clock")) {
-			System.out.println("You approach the cat clock. It's avoiding your gaze. You watch for a while, and, despite the motion of the second hand, the time doesn't change.\n"
+			System.out.println("\nYou approach the cat clock. It's avoiding your gaze. You watch for a while, and, despite the motion of the second hand, the time doesn't change.\n"
 					+ "10:15 pm. If you say so. You turn back to the rest of the room.\n");
+			System.out.println(ITEMINTERACT);
 			userInput = scnr.nextLine();
 			door3Interact(userInput);
 		}
 		
 		else if (investigate.contains("pool")) {
-			System.out.println("You lower yourself down into the pool. Ooh! Fuzzy! Squishy! The kitties are so cute!\n"
+			System.out.println("\nYou lower yourself down into the pool. Ooh! Fuzzy! Squishy! The kitties are so cute!\n"
 					+ "You spend a few minutes rolling around. You get out feeling refreshed and glad no one is watching.\n");
+			System.out.println(ITEMINTERACT);
 			userInput = scnr.nextLine();
 			door3Interact(userInput);
 		}
 		
 		else if (investigate.contains("hole")) {
-			System.out.println("You look into the hole. Your face blocks the light. You see nothing.\n"
+			System.out.println("\nYou look into the hole. Your face blocks the light. You see nothing.\n"
 					+ "You put your ear to the hole. You think you hear faint, distant meowing. Ominous.\n"
 					+ "You stand back up.\n");
+			System.out.println(ITEMINTERACT);
 			userInput = scnr.nextLine();
 			door3Interact(userInput);
 		}
@@ -58,27 +61,37 @@ public class DoorParty {
 		else if (investigate.contains("trivia")) {
 			//CALL TRIVIA STUFF IN TRIVIA CLASS
 		}
-		
-		else if (investigate.contains("exit")) {
-			System.out.println("You leave the room.\n");
-			FinalMain.doorIntro();
+
+		else if (investigate.contains("help")){
+			System.out.println(HellStrings.HELP);
+			userInput = scnr.nextLine();
+			door3Interact(userInput);
 		}
 		
-		else {
-			System.out.println("Huh? Do you want a reminder of your options?");
+		else if (investigate.contains("exit")) {
+			System.out.println("\nYou leave the room.\n");
+			FinalMain.doorIntro();
+		}
+
+		else if (investigate.contains("room")) {
+			System.out.println("\nHuh? Do you need me to describe the room AGAIN??");
 			userInput = scnr.nextLine();
 			
 			if (userInput.charAt(0) == 'y') {
+				System.out.println("\nOkay... be sure to listen this time");
 				door3Cycle();
-			}
-			
-			else {
-				System.out.println();
-				userInput = scnr.nextLine();
-				door3Interact(userInput);
+			}else{
+				System.out.println("\nWell well SOMEbody is indecisive today aren't they..\n");
+				door3Interact(userInput);	
 			}
 		}
-	}
+		else {
+			System.out.println("\nI didn't understand that (remember enter 'help' to bring up a list of things you can do)");
+			userInput = scnr.nextLine();
+			door3Interact(userInput);
+			}
+		}
+	
 	
 	public static void door3Cycle() throws FileNotFoundException {
 		System.out.print(ITEMPROMPT);
--- gamefinal/gamefinal/HellStrings.java
+++ gamefinal/gamefinal/HellStrings.java
@@ -6,13 +6,13 @@ public class HellStrings {
 	static Scanner scnr = new Scanner(System.in);
 	final static String GAMEINTRO1 = "! This is a trivia-based adventure game. "
 			+ "There are four rooms available, each with trivia on a different topic.\n"
-			+ "You have to answer all the questions before you can get to the final room.\n";
+			+ "You have to answer all the questions before you can get to the final room. (Press enter to continue)\n";
 	final static String GAMEINTRO2 = "Each question will allow you to bet some of your hard-earned COINS on the answer.\n"
 			+ "[INSERT EXPLANATION OF HOW THAT WORKS]. You have 10 coins in your pocket right now.\n"
 			+ "What, where did you get them?\nHow should I know! You have a job, don't you?\n";
 	final static String JOKESONYOU = "I'm kidding, I don't care.\n";
-	final static String HOWTOPLAY = "At any time you're prompted to give input, you can SAVE your game, QUIT your game, RESTART your game, or see your INVENTORY.\n"
-			+ "Those commands are, respectively, 'save', 'quit', 'restart', and 'inventory'. Why don't you try opening your inventory?\n";
+	final static String HOWTOPLAY = "At any time you're prompted to give input, you can SAVE your game, QUIT your game, RESTART your game, see your INVENTORY or get HELP\n"
+			+ "Those commands are, respectively, 'save', 'quit', 'restart', 'inventory' and 'help'. Why don't you try opening your inventory?\n";
 	final static String WHERETONOW = "There are four doors in front of you.\n"
 			+ "Door 1:\n"
 			+ "Door 2:\n"
@@ -20,7 +20,10 @@ public class HellStrings {
 			+ "From a certain angle, it looks kind of like a Pop-tart.\n"
 			+ "The handle is a silver curl, like an unwinding ball of yarn, and extends half the door's width. "
 			+ "Weird.\n"
-			+ "Door 4:\n"; //EVERYONE DESCRIBE YOUR DOORS
+			+ "Door 4:\n" //EVERYONE DESCRIBE YOUR DOORS
+			+ "\nWhich Door would you like to enter? [1-4]\n";
+	final static String HELP = "\nHere are some things you can do:\nsave\nquit\nrestart\ninventory\nroom\ntrivia\nif you want to examine something, try typing it's name. "; 
+								//^make sure this is ubiquitious for all situations
 			
 			
 	public static void getGameIntro() {
@@ -38,9 +41,7 @@ public class HellStrings {
 	public static void doorMenu() {
 		System.out.print(WHERETONOW);
 		scnr.nextLine();
-		System.out.println("Which number would you like to enter?");
 	}
-	
 	
 	
 	public HellStrings() {
